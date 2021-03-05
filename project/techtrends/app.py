2f0e889167dbcf695a3b0f4d09ecf645b524de1c
hussain poonawala <hussain.poonawala@gdn-commerce.com>
2021-03-06 00:06:30 +0530

    Imported sys

--- project/techtrends/app.py
+++ project/techtrends/app.py
@@ -2,7 +2,7 @@ import sqlite3
 
 from flask import Flask, jsonify, json, render_template, request, url_for, redirect, flash
 from werkzeug.exceptions import abort
-import logging
+import logging, sys
 
 COUNTER = 0
 # Function to get a database connection.
